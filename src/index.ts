be7f774e0a2603dfd6fb5cf814773db8c88d4462
Dmytro <dmytro.osiychuk@gmail.com>
2022-02-18 15:06:27 +0100

    Refactor using composition approach

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
 
-const reader = new CsvFileReader('football.csv');
-reader.read();
-
-//enum
+//create object to satisfy DataReader interface
+const csvFileReader = new CsvFileReader('football.csv');
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'ManUnited' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
