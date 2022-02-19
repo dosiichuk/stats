c87b3cf27ef2fb2a125ab7d8c18ed4071a1d5108
Dmytro <dmytro.osiychuk@gmail.com>
2022-02-19 18:42:40 +0100

    Refactor using composition approach

---
+++ src/Summary.ts
@@ -0,0 +1,22 @@
+import { MatchData } from './MatchData';
+import { WinsAnalysis } from './Analyzers/WinsAnalysis';
+import { HtmlReports } from './reportTargets/HtmlReports';
+
+export interface Analyzer {
+  run(matches: MatchData[]): string;
+}
+
+export interface OutputTarget {
+  print(report: string): void;
+}
+
+export class Summary {
+  static winsAnalysisWithHtmlReport(team: string): Summary {
+    return new Summary(new WinsAnalysis(team), new HtmlReports());
+  }
+  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
-import { CsvFileReader } from './CsvFileReader';
 
-//create object to satisfy DataReader interface
-const csvFileReader = new CsvFileReader('football.csv');
-const matchReader = new MatchReader(csvFileReader);
+import { Summary } from './Summary';
+
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 matchReader.load();
 
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-  if (match[1] === 'ManUnited' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-console.log(`Man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
